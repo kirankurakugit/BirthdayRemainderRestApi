b8c57748b93938fd1a93d91ada7b24645c23b800
kirankurakugit <42097142+kirankurakugit@users.noreply.github.com>
2019-09-19 15:45:21 +0530

    updated Dao

--- src/main/java/com/mkyong/form/dao/UserDao.java
+++ src/main/java/com/mkyong/form/dao/UserDao.java
@@ -1,19 +1,12 @@
-package com.mkyong.form.dao;
+package com.zensar.form.dao;
 
 import java.util.List;
 
 import com.mkyong.form.model.User;
 
 public interface UserDao {
-
-	User findById(Integer id);
-
-	List<User> findAll();
-
+	
 	void save(User user);
 
-	void update(User user);
-
-	void delete(Integer id);
-
+		
 }
