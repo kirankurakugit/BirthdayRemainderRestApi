5e33362ce5217a600e2d13df223d22d58d977d8c
kirankurakugit <42097142+kirankurakugit@users.noreply.github.com>
2019-09-19 17:05:33 +0530

    commit to formcontroller

--- src/main/java/com/mkyong/form/web/UserFormController.java
+++ src/main/java/com/mkyong/form/web/UserFormController.java
@@ -1,4 +1,4 @@
-/*package com.mkyong.form.web;
+/*package com.zensar.form.web;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,8 +16,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.mkyong.form.model.User;
-import com.mkyong.form.service.UserService;
+import com.zensasr.form.model.User;
+import com.zensar.form.service.UserService;
 
 @Controller
 public class UserFormController {
@@ -55,75 +55,21 @@ public class UserFormController {
 		User user = new User();
 		
 		//set default value
-		user.setName("mkyong");
-		user.setEmail("test@gmail.com");
-		user.setAddress("abc 88");
-		user.setNewsletter(true);
-		user.setSex("M");
-		user.setFramework(new ArrayList<String>(Arrays.asList("Spring MVC", "GWT")));
-		user.setSkill(new ArrayList<String>(Arrays.asList("Spring", "Grails")));
-		user.setCountry("SG");
-		user.setNumber(2);
+		user.setuserName("kiran");
+		user.setBirthdayDate("02-02-1985");
+		
 		
 		model.addAttribute("userFormModel", user);
-		model.addAttribute("title", "Add User");
-		model.addAttribute("action", "Add");
+		model.addAttribute("message", "birthdayremainder");
 		
 		return "user/userform";
 
 	}
 
-	// like jUnit @BeforeMethod, run before every @RequestMapping in this @Controller
-	@ModelAttribute
-	public void populateAllDropDown(Model model) {
-
-		logger.debug("populateAllDropDown()");
-
-		List<String> frameworksList = new ArrayList<String>();
-		frameworksList.add("Spring MVC");
-		frameworksList.add("Struts 2");
-		frameworksList.add("JSF 2");
-		frameworksList.add("GWT");
-		frameworksList.add("Play");
-		frameworksList.add("Apache Wicket");
-		model.addAttribute("frameworkList", frameworksList);
 	
-		Map<String,String> skill = new LinkedHashMap<String,String>();
-		skill.put("Hibernate", "Hibernate");
-		skill.put("Spring", "Spring");
-		skill.put("Struts", "Struts");
-		skill.put("Groovy", "Groovy");
-		skill.put("Grails", "Grails");
-		model.addAttribute("javaSkillList", skill);
-		
-	}
 
-	@ModelAttribute("numberList")
-	public List<Integer> populateNumberList() {
-
-		logger.debug("populateNumberDropDown()");
-
-		List<Integer> numbers = new ArrayList<Integer>();
-		numbers.add(1);
-		numbers.add(2);
-		numbers.add(3);
-		numbers.add(4);
-		numbers.add(5);
-
-		return numbers;
-
-	}
 	
-	@ModelAttribute("countryList")
-	public Map<String,String> populateCountryList() {
-
-		Map<String,String> country = new LinkedHashMap<String,String>();
-		country.put("US", "United Stated");
-		country.put("CN", "China");
-		country.put("SG", "Singapore");
-		country.put("MY", "Malaysia");
-		return country;
-
-	}
+	
+	
 	
 }*/
