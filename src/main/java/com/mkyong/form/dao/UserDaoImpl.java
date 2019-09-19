4f88267df64d61980a6cb25f8439fc440a2fc01f
kirankurakugit <42097142+kirankurakugit@users.noreply.github.com>
2019-09-19 15:51:14 +0530

    Update DaoImpl

--- src/main/java/com/mkyong/form/dao/UserDaoImpl.java
+++ src/main/java/com/mkyong/form/dao/UserDaoImpl.java
@@ -32,91 +32,26 @@ public class UserDaoImpl implements UserDao {
 		this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
 	}
 
-	@Override
-	public User findById(Integer id) {
-
-		Map<String, Object> params = new HashMap<String, Object>();
-		params.put("id", id);
-
-		String sql = "SELECT * FROM users WHERE id=:id";
-
-		User result = null;
-		try {
-			result = namedParameterJdbcTemplate.queryForObject(sql, params, new UserMapper());
-		} catch (EmptyResultDataAccessException e) {
-			// do nothing, return null
-		}
-
-		/*
-		 * User result = namedParameterJdbcTemplate.queryForObject( sql, params,
-		 * new BeanPropertyRowMapper<User>());
-		 */
-
-		return result;
-
-	}
-
-	@Override
-	public List<User> findAll() {
-
-		String sql = "SELECT * FROM users";
-		List<User> result = namedParameterJdbcTemplate.query(sql, new UserMapper());
-
-		return result;
-
-	}
+	
 
 	@Override
 	public void save(User user) {
 
-		KeyHolder keyHolder = new GeneratedKeyHolder();
 		
-		String sql = "INSERT INTO USERS(NAME, EMAIL, ADDRESS, PASSWORD, NEWSLETTER, FRAMEWORK, SEX, NUMBER, COUNTRY, SKILL) "
-				+ "VALUES ( :name, :email, :address, :password, :newsletter, :framework, :sex, :number, :country, :skill)";
-
-		namedParameterJdbcTemplate.update(sql, getSqlParameterByModel(user), keyHolder);
-		user.setId(keyHolder.getKey().intValue());
+		
+		String sql = "INSERT INTO Birthday(USERNAMENAME, BIRTHDAYDATEL) "
+				+ "VALUES ( :username, :birthdaydate)";
 		
 	}
-
-	@Override
-	public void update(User user) {
-
-		String sql = "UPDATE USERS SET NAME=:name, EMAIL=:email, ADDRESS=:address, " + "PASSWORD=:password, NEWSLETTER=:newsletter, FRAMEWORK=:framework, "
-				+ "SEX=:sex, NUMBER=:number, COUNTRY=:country, SKILL=:skill WHERE id=:id";
-
-		namedParameterJdbcTemplate.update(sql, getSqlParameterByModel(user));
-
-	}
-
-	@Override
-	public void delete(Integer id) {
-
-		String sql = "DELETE FROM USERS WHERE id= :id";
-		namedParameterJdbcTemplate.update(sql, new MapSqlParameterSource("id", id));
-
-	}
+	
 
 	private SqlParameterSource getSqlParameterByModel(User user) {
-
-		// Unable to handle List<String> or Array
-		// BeanPropertySqlParameterSource
-
+		
 		MapSqlParameterSource paramSource = new MapSqlParameterSource();
 		paramSource.addValue("id", user.getId());
-		paramSource.addValue("name", user.getName());
-		paramSource.addValue("email", user.getEmail());
-		paramSource.addValue("address", user.getAddress());
-		paramSource.addValue("password", user.getPassword());
-		paramSource.addValue("newsletter", user.isNewsletter());
-
-		// join String
-		paramSource.addValue("framework", convertListToDelimitedString(user.getFramework()));
-		paramSource.addValue("sex", user.getSex());
-		paramSource.addValue("number", user.getNumber());
-		paramSource.addValue("country", user.getCountry());
-		paramSource.addValue("skill", convertListToDelimitedString(user.getSkill()));
-
+		paramSource.addValue("username", user.getuserName());
+		paramSource.addValue("birthdaydate", user.getbirthdayDate());
+		
 		return paramSource;
 	}
 
@@ -124,40 +59,13 @@ public class UserDaoImpl implements UserDao {
 
 		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
 			User user = new User();
-			user.setId(rs.getInt("id"));
-			user.setName(rs.getString("name"));
-			user.setEmail(rs.getString("email"));
-			user.setFramework(convertDelimitedStringToList(rs.getString("framework")));
-			user.setAddress(rs.getString("address"));
-			user.setCountry(rs.getString("country"));
-			user.setNewsletter(rs.getBoolean("newsletter"));
-			user.setNumber(rs.getInt("number"));
-			user.setPassword(rs.getString("password"));
-			user.setSex(rs.getString("sex"));
-			user.setSkill(convertDelimitedStringToList(rs.getString("skill")));
+			user.setBirthdayDated(rs.getBirthdayDate("birthdaydate"));
+			user.setName(rs.getString("username"));
+			
 			return user;
 		}
 	}
 
-	private static List<String> convertDelimitedStringToList(String delimitedString) {
-
-		List<String> result = new ArrayList<String>();
-
-		if (!StringUtils.isEmpty(delimitedString)) {
-			result = Arrays.asList(StringUtils.delimitedListToStringArray(delimitedString, ","));
-		}
-		return result;
-
-	}
-
-	private String convertListToDelimitedString(List<String> list) {
-
-		String result = "";
-		if (list != null) {
-			result = StringUtils.arrayToCommaDelimitedString(list.toArray());
-		}
-		return result;
-
-	}
+	
 
 }
